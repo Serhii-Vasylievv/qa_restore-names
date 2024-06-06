58f2f0426b0bd07d8f8de93d70815e110f4850cf
Serhii Vasyliev <vv44980@gmail.com>
2024-06-05 22:38:32 -0600

    tests are done

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,54 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
 
+  it(`The function should return 'firstName' from the fullName'`, () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Jack');
   });
 
-  // write tests here
+  it(`if 'firstName' has undefined`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Jack');
+  }); 
+  
+  it(`should check if the 'FirstName' had two words`, () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: 'Muhamed Aly Boxer',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].firstName).toBe('Muhamed Aly');
+  });
+
+  it(`should return error message if the 'fullName' is empty`, () => {
+    const users = [
+      {
+        firstName: '',
+        lastName: 'Holy',
+        fullName: '',
+      },
+    ];
+    restoreNames(users);
+    expect(users[0].fullName).toThrow('fullName is empty');
+  });
 });
